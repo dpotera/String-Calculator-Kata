c6c1ae533e5360834fbec84dfb7edcd89a4673da
Dominik Potera <dominik.potera@gmail.com>
2019-07-08 22:34:58 +0200

    Case 3

--- src/main/java/pl/potera/stringcalc/StringCalculator.java
+++ src/main/java/pl/potera/stringcalc/StringCalculator.java
@@ -10,12 +10,26 @@ public class StringCalculator implements Calculator {
             return 0;
         }
         return split(numbers)
+                .map(this::validate)
                 .map(Integer::parseInt)
                 .reduce(Integer::sum)
                 .orElse(0);
     }
 
     private Stream<String> split(String input) {
-        return Arrays.stream(input.split(","));
+        return splitToStream(input, "\n")
+                .flatMap(line -> splitToStream(line, ","));
+    }
+
+    private Stream<String> splitToStream(String input, String delimiter) {
+        return Arrays.stream(input.split(delimiter, -1));
+    }
+
+    private String validate(String line) {
+        if (line.isEmpty()) {
+            throw new IllegalArgumentException();
+        } else {
+            return line;
+        }
     }
 }
--- src/test/java/pl/potera/stringcalc/StringCalculatorSpec.groovy
+++ src/test/java/pl/potera/stringcalc/StringCalculatorSpec.groovy
@@ -17,12 +17,26 @@ class StringCalculatorSpec extends Specification {
         calculator.add(input) == result
 
         where:
-        input       | result
-        ""          | 0
-        "1"         | 1
-        "1,2"       | 3
-        "1,2,10"    | 13
-        "1,2,4,5,2" | 14
+        input           | result
+        ""              | 0
+        "1"             | 1
+        "1,2"           | 3
+        "1,2,10"        | 13
+        "1,2,4,5,2"     | 14
+        "1,2\n4\n5,2"   | 14
+        "1\n4\n5"       | 10
+        "1,\n"       | 10
+    }
+
+    def "should throw IllegalArgumentException for invalid input"() {
+        when:
+        calculator.add(input)
+
+        then:
+        thrown(IllegalArgumentException)
+
+        where:
+        input  << ["1,\n", "\n"]
     }
 
     def "should handle null input"() {
